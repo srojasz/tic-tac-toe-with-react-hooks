d8092e27a615b1bdd77c73bb44627502eb6bcec9
srojasz <srojasz@gmail.com>
2021-02-28 19:47:55 +0100

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,14 @@ describe("Board", () => {
     const squares = ["x", "x", "x", null, "o", "o", "x", "o", null];
     const handleClick = jest.fn();
     render(
-      <Board handleClick={handleClick} squares={squares} disabled="false" />
+      <Board handleClick={handleClick} squares={squares} disabled={true} />
     );
   });
+  // test("Board has 9 squares", () => {
+  //   const squares = document.QuerySelectorAll("[data-js-square]");
+  //   const squaresNum = squares.length;
+  //   expect(squaresNum).toEqual(9);
+  // });
 });
 
 // test("renders Board and finds 9 squares", () => {
